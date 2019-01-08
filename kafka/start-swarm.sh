371c14c79217ee55bd0a875046a4cef74401d643
Stephen Rufle <srufle@gmail.com>
2019-01-08 12:48:50 -0700

    Changed to creatre tmp dir during swarm creation

--- kafka/start-swarm.sh
+++ kafka/start-swarm.sh
@@ -4,6 +4,7 @@ ADVERTISE_ADDR=$(ip addr show ${INTERFACE} | grep -Po 'inet \K[\d.]+')
 docker swarm init --advertise-addr "${ADVERTISE_ADDR}"
 
 CMD_ROOT="/vagrant"
+mkdir -p "${CMD_ROOT}/tmp/"
 JOIN_CMD=$(docker swarm join-token worker | grep token)
 echo "#!/bin/bash" > "${CMD_ROOT}/tmp/join-swarm.sh"
 echo "${JOIN_CMD}" >> "${CMD_ROOT}/tmp/join-swarm.sh"
